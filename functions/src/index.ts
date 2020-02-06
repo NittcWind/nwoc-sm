643aae41d57a70f9b1b10bb94e7e6d29a032f688
ssssota <tommysouta@gmail.com>
2020-02-06 14:19:48 +0900

    add pdf endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,9 @@ import * as admin from 'firebase-admin'
 import * as os from 'os'
 import * as path from 'path'
 import * as fs from 'fs'
+import * as PdfDocument from 'pdfkit'
+const PdfTable = require('voilab-pdf-table')
+const Fitcolumn = require('voilab-pdf-table/plugins/fitcolumn')
 const jconv = require('jconv')
 
 //const serviceAccount = require('./secret-key.json')
@@ -68,7 +71,7 @@ const makeScoreList = () => new Promise<IScoreList>(async (resolve, reject) => {
       if (address === '課題曲') {
         year = data.year
       } else {
-        publisher = publishers[data.publisher.id]
+        publisher = (data.publisher)? publishers[data.publisher.id]: ''
       }
       const score: IScore = {
         name: data.name,
@@ -199,6 +202,67 @@ exports.csv = functions.https.onRequest(async (req, res) => {
   res.write(jconv.convert(csv, 'UTF8', 'SJIS'))
   res.status(200)
   res.end()
+})
+
+// /pdf にアクセスしてPDFファイル取得
+exports.pdf = functions.https.onRequest(async (req, res) => {
+  const title = '豊田高専吹奏楽部 楽譜一覧'
+  const pdf = new PdfDocument({
+    size: 'A4',
+    layout: 'landscape',
+    margin: 40,
+    info: {
+      Creator: 'nwoc Score Manager',
+      CreationDate: new Date(),
+      Title: title,
+      Author: 'nwoc-sm'
+    },
+    bufferPages: true
+  })
+  const table = new PdfTable(pdf)
+  const scores = await getScoreList()
+  //interface I { [key: string]: { address: string; name: string; } }
+  //const scores = {"09Qv9qQ9IYomRkwj7OKe":{"address":"E","name":"Elsa's Procession to the Cathedral","note":"エルザの大聖堂への行進（行列）","otherName":"","publisher":"その他","singer":""},"0B1syPoggNmmHgunVine":{"address":"R","name":"READY!!","note":"","otherName":"","publisher":"その他","singer":""},"0ZhoyM3NrYUqVzxqTCPT":{"address":"課題曲","name":"マーチ「ライジング・サン」","note":"新井千悦子　作曲","otherName":"March \"The rising sun\"","singer":"","year":"1997"},"0kxkyrNVPahoC94fxCon":{"address":"D","name":"Dirty Work","note":"郷間幹男　他\nブルゾンちえみ","otherName":"","publisher":"ウィンズスコア","singer":"Austin Mahone"},"0t8JGzjw5AAqlTsNPqcR":{"address":"課題曲","name":"平和への行列","note":"戸田顕　作曲","otherName":"","singer":"","year":"2001"},"0wXacncV9eDczrVGcJCa":{"address":"A","name":"A Child's Lullaby","note":"","otherName":"","publisher":"その他","singer":""},"15M81OrTotgHjkXn2p8b":{"address":"M","name":"MIDWAY MARCH","note":"John Williams","otherName":"ミッドウェイ・マーチ","publisher":"その他","singer":""},"16sdmYhdKg4Ys0HE5nLm":{"address":"課題曲","name":"アップル・マーチ","note":"野村正憲　作曲","otherName":"","singer":"","year":"1995"},"1ADh2lxzKdGdfX3NdKci":{"address":"課題曲","name":"スター・パズル・マーチ","note":"小長谷宗一　作曲","otherName":"","singer":"","year":"1993"},"1CCdFv1rORs2qTH6JJvw":{"address":"E","name":"EL CAMINO REAL","note":"","otherName":"エル・カミーノ・レアル","publisher":"その他","singer":""},"1ILyvDQxnF6wHE82cVKK":{"address":"H","name":"HERO -Main Title-2014","note":"ドラマ「Hero」\n木村拓哉","otherName":"","publisher":"ウィンズスコア","singer":""},"1LBQrdqJwH5WX2fvrK8v":{"address":"M","name":"MAMMA MIA!","note":"Highlights From the Broadway Musical","otherName":"マンマ・ミーア!","publisher":"その他","singer":""},"1OXJ8uq9SmsTnH0sJ7RM":{"address":"C","name":"Coppelia 2nd Suite","note":"","otherName":"","publisher":"その他","singer":""},"1V2OpuxO3yFJwxhC5v8m":{"address":"D","name":"Dragon Night","note":"","otherName":"","publisher":"ウィンズスコア","singer":"SEKAI NO OWARI"},"1WPW8Rh9lZ34PV8SeWFw":{"address":"S","name":"SEPTEMBER","note":"","otherName":"","publisher":"その他","singer":""},"1a59nXEwXOHTBym0bZmA":{"address":"課題曲","name":"吹奏楽のための序曲 南の島から -沖縄旋律による-","note":"服部公一　作曲","otherName":"","singer":"","year":"1980"},"1ejWk15HXToBH4NREC2m":{"address":"S","name":"STAR WARS (Main Thema)","note":"","otherName":"","publisher":"その他","singer":""},"1kl7LBlNUCci1IZCHcin":{"address":"課題曲","name":"16世紀のシャンソンによる変奏曲","note":"諏訪　雅彦　作曲","otherName":"Variations on a 16th Century Chanson","singer":"","year":"2009"},"1ruo0wZvoi1eInZxqGwh":{"address":"課題曲","name":"行進曲 オーバー・ザ・ギャラクシー （銀河を越えて）","note":"斎藤高順　作曲","otherName":"","singer":"","year":"1980"},"21khS8wyiObaspyA8Hhq":{"address":"T","name":"THE WAY WE WERE 追憶のテーマ","note":"","otherName":"","publisher":"その他","singer":""},"2GndyGn7A1DDk1GbLt9f":{"address":"I","name":"I AM YOUR SINGER","note":"","otherName":"","publisher":"ミュージックエイト","singer":"サザンオールスターズ"},"2TiMOljHLGsCotDFgb5O":{"address":"S","name":"SYMPHONIC MOVEMENT","note":"","otherName":"","publisher":"その他","singer":""},"2fYpJ7zyImPvP55P966j":{"address":"O","name":"OLYMPICA","note":"Festival Overture for Band","otherName":"","publisher":"その他","singer":""},"3GiYBaZCRDUPvB8Hs9cJ":{"address":"課題曲","name":"吹奏楽のための序曲","note":"間宮芳生　作曲","otherName":"","singer":"","year":"1986"},"3Sxg0m0fq0g4gjcx7NOk":{"address":"P","name":"Pecori Night","note":"","otherName":"","publisher":"その他","singer":""},"3aCuDk8kzffYMsjrEBFP":{"address":"P","name":"PICTURES AT AN EXHIBITION PartⅢ","note":"","otherName":"","publisher":"その他","singer":""},"3hVqrHUAi7tjBB9sGflX":{"address":"課題曲","name":"吹奏楽のためのラメント","note":"高昌師　作曲","otherName":"","singer":"","year":"2002"},"3ltBV36DWkVUxMUPQogM":{"address":"B","name":"Believe","note":"","otherName":"","publisher":"ミュージックエイト","singer":""},"3xWvhK7GtgU5nDDKeBIv":{"address":"H","name":"Hungarian Rhapsody No.2","note":"","otherName":"ハンガリー狂詩曲第2番","publisher":"その他","singer":""},"414KSjHpwuJtfucyoe0U":{"address":"課題曲","name":"鳥たちの神話","note":"藤井修　作曲","otherName":"","singer":"","year":"2004"},"475NkE4PSLoIBKfP2ZUa":{"address":"L","name":"LES COULEURS FAUVES","note":"","otherName":"レ・クルール・フォーヴ","publisher":"その他","singer":""},"4YjtgFFdLcWv9esFSUR4":{"address":"J","name":"JAPANESE FOLK SONG SUITE WARABE UTA","note":"Where Are You From? あんたがたどこさ\nLullaby 子守歌\nAn Ancient Priest in a Moutain Temple 山寺のお尚さん","otherName":"わらべ唄","publisher":"その他","singer":""},"4aIeSO6wzvf6AKEAXobB":{"address":"P","name":"PACHELBEL'S CANON","note":"","otherName":"","publisher":"その他","singer":""},"4cboJeQerofeKxpnusZ5":{"address":"課題曲","name":"マーチ「カタロニアの栄光」","note":"間宮芳生　作曲","otherName":"","singer":"","year":"1990"},"4pFt4ZJKJo9JE0nRSt7I":{"address":"P","name":"Priestermars uit,,Athalia\"","note":"","otherName":"","publisher":"その他","singer":""},"4y3URCvWw9kCkaw0u1gR":{"address":"課題曲","name":"吹奏楽のための一章","note":"堀内俊男　作曲","otherName":"","singer":"","year":"2006"},"524PPfsErpeUFY3y7JDB":{"address":"課題曲","name":"天空の旅－吹奏楽のための譚詩－","note":"石原勇太郎","otherName":"Pilgrimage - Ballade for wind orchestra","singer":"","year":"2015"}} as I
+
+  pdf.font('fonts/migmix-2p-regular.ttf')
+  pdf.fontSize(10.5)
+  table.addPlugin(new Fitcolumn({
+    column: 'note'
+  }))
+  table.setColumnsDefaults({
+    headerBorder: 'B',
+    border: 'B'
+  })
+  table.addColumns([
+    { id: 'name', header: '正式名', width: 200 },
+    { id: 'otherName', header: '別名', width: 200 },
+    { id: 'address', header: '保管場所', width: 50 },
+    { id: 'singer', header: '歌手', width: 100 },
+    { id: 'note', header: '備考' }
+  ])
+  table.onPageAdded((tbl: any) => tbl.addHeader())
+  table.addBody(Object.keys(scores).map(key => scores[key]).sort((a,b) => {
+    const addressA = a.address
+    const addressB = b.address
+    if (addressA < addressB) return -1
+    if (addressA > addressB) return 1
+    const nameA = a.name
+    const nameB = b.name
+    if (nameA < nameB) return -1
+    if (nameA > nameB) return 1
+    return 0
+  }))
+
+  const { start, count } = pdf.bufferedPageRange()
+  const date = new Date()
+  for (let i = start, end = start + count; i < end; i++) {
+    pdf.switchToPage(i)
+    pdf.text(title, 30, 20)
+    pdf.text(`${date.toLocaleDateString('jp')}`, pdf.page.width - 100, 20)
+    pdf.text(`${i+1}/${count}`, pdf.page.width - 100, 30)
+  }
+  pdf.pipe(res)
+  pdf.end()
 })
 
 // Firestoreの変更（楽譜の追加・編集）検知
