aa1bff3f3bbfdb02993f01e7f86c4a4fabf483a1
ssssota <tommysouta@gmail.com>
2020-02-06 20:43:53 +0900

    set pwa options

--- nwoc-sm/vue.config.js
+++ nwoc-sm/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: 'nwoc Score Manager',
+    themeColor: '#ff980f',
+    msTileColor: '#4d4d4d',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      short_name: 'nwoc-sm'
+    }
+  }
 }
