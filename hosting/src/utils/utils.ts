ca09e798d4a29aa2d7fc0f8cbab5f7ff2ca92ea6
Sotaro Tommykawa <tommysouta@gmail.com>
2020-09-11 23:19:16 +0900

    Add publisers getter

--- hosting/src/utils/utils.ts
+++ hosting/src/utils/utils.ts
@@ -11,11 +11,22 @@ export const getUrlParam = (() => {
 
 export const addresses = firestore
   .collection('addresses')
-  .orderBy('address')
-  .get().then(querySnapshot => {
+  .orderBy('address').get()
+  .then(querySnapshot => {
     const addresses = new Map<string, string>()
     querySnapshot.forEach(doc => {
       addresses.set(doc.id, doc.data().address as string)
     })
     return addresses
+  })
+
+export const publisers = firestore
+  .collection('publishers')
+  .orderBy('name').get()
+  .then(querySnapshot => {
+    const publishers = new Map<string, string>();
+    querySnapshot.forEach(doc => {
+      publishers.set(doc.id, doc.data().name)
+    })
+    return publishers
   })
