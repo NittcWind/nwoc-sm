3ef384e78a9561fece7c5b3eb84d07a74068aeaf
Sotaro Tommykawa <tommysouta@gmail.com>
2020-09-11 23:16:44 +0900

    Add addresses getter

--- hosting/src/utils/utils.ts
+++ hosting/src/utils/utils.ts
@@ -1,8 +1,21 @@
+import { firestore } from './firebase'
+
 export const getUrlParam = (() => {
   const params = new URL(window.location.href).searchParams
   return (key: string) => {
     const result = params.get(key)
     if (result == null) return null
-    return window.decodeURIComponent(result)
+    return decodeURIComponent(result)
   }
 })()
+
+export const addresses = firestore
+  .collection('addresses')
+  .orderBy('address')
+  .get().then(querySnapshot => {
+    const addresses = new Map<string, string>()
+    querySnapshot.forEach(doc => {
+      addresses.set(doc.id, doc.data().address as string)
+    })
+    return addresses
+  })
