345cd312690fda26036a595d1157137dfa857427
Sotaro Tommykawa <tommysouta@gmail.com>
2020-09-12 21:45:32 +0900

    Add desc/asc button

---
+++ hosting/src/components/SortDescButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-btn
+    icon
+    @click="$emit('change', !value)"
+  >
+    <v-icon v-if="value">
+      {{ icons.mdiSortDescending }}
+    </v-icon>
+    <v-icon v-else>
+      {{ icons.mdiSortAscending }}
+    </v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Model, Vue } from 'vue-property-decorator'
+import { mdiSortDescending, mdiSortAscending } from '@mdi/js'
+
+@Component
+export default class SortDescButton extends Vue {
+  @Model('change', { required: true }) value!: boolean;
+
+  icons = {
+    mdiSortDescending,
+    mdiSortAscending
+  }
+}
+</script>
